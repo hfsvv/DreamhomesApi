8e97e8dde1dc92a014a045b220240091f9ad300d
hfsvv <77497064+hfsvv@users.noreply.github.com>
2021-06-28 02:47:34 +0530

    Update settings.py

--- propertysellapp/settings.py
+++ propertysellapp/settings.py
@@ -129,16 +129,16 @@ USE_TZ = True
 # STATICFILES_DIRS=[
 #     os.path.join(BASE_DIR,'static')
 # ]
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 AUTH_USER_MODEL="ads.Userinfo"
